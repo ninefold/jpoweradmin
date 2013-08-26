04c76f63c4b41d03cd7989c3e4c59cc23d32a19e
Shaun Domingo <shaun@ninefold.com>
2013-08-26 14:50:53 +1000

    Remove newer version of JPowerAdmin - go to v0.99

--- resources/import.sql
+++
@@ -1,8 +0,0 @@
--- SQL statements which are executed at application startup if hibernate.hbm2ddl.auto is 'create' or 'create-drop'
---CREATE UNIQUE INDEX user_name_idx ON `user` (userName);
---CREATE UNIQUE INDEX name_index ON domains(name);
---CREATE INDEX rec_name_index ON records(name);
-CREATE INDEX nametype_index ON records(name,`type`);
-
-
-
